d1d4d828059d981b6c9d121be89d887e97249f3a
Leprikon <Zotchik.e.i@gmail.com>
2025-05-09 12:43:42 +0300

    Initial Commit

---
+++ Sudoky/SudokyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokyApp.swift
+//  Sudoky
+//
+//  Created by Евгений Зотчик on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SudokyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
